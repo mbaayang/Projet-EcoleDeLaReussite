a8f3c12b1b3611563b5bea981f834ad6eccf291b
Mbayang Gueye <mbayangueye07@gmail.com>
2022-10-03 16:44:13 +0000

    inscription

--- connexion.php
+++ connexion.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="la_reussite.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/431fa92df2.js" crossorigin="anonymous"></script>
     <title>Connexion</title>
 </head>
 <body>
@@ -65,5 +67,6 @@
     <?php
     include('footer.php')
     ?>
+    <script src="la_reussite.js"></script>
 </body>
 </html>
--- inscription_employes.php
+++ inscription_employes.php
@@ -6,6 +6,8 @@
             <meta name="author" content="NoS1gnal"/>
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
             <link rel="stylesheet" href="la_reussite.css">
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+            <script src="https://kit.fontawesome.com/431fa92df2.js" crossorigin="anonymous"></script>
             <title>Inscription</title>
         </head>
         <body>
