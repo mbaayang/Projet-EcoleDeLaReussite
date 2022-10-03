b9cd3bb04d0ca265664a703928ab84c8bb40afc4
Mbayang Gueye <mbayangueye07@gmail.com>
2022-10-03 17:57:26 +0000

    inscription

--- inscription.php
+++ inscription.php
@@ -19,70 +19,17 @@
         <body>
         <div class="login-form">
             <?php 
-                if(isset($_GET['reg_err']))
-                {
-                    $err = htmlspecialchars($_GET['reg_err']);
-
-                    switch($err)
-                    {
-                        case 'success':
-                        ?>
-                            <div class="alert alert-success">
-                                <strong>Succès</strong> inscription réussie !
-                            </div>
-                        <?php
-                        break;
-
-                        case 'password':
-                        ?>
-                            <div class="alert alert-danger">
-                                <strong>Erreur</strong> mot de passe différent
-                            </div>
-                        <?php
-                        break;
-
-                        case 'email':
-                        ?>
-                            <div class="alert alert-danger">
-                                <strong>Erreur</strong> email non valide
-                            </div>
-                        <?php
-                        break;
-
-                        case 'email_length':
-                        ?>
-                            <div class="alert alert-danger">
-                                <strong>Erreur</strong> email trop long
-                            </div>
-                        <?php 
-                        break;
-
-                        case 'statut_length':
-                        ?>
-                            <div class="alert alert-danger">
-                                <strong>Erreur</strong> statut trop long
-                            </div>
-                        <?php 
-                        break;
-                        case 'already':
-                        ?>
-                            <div class="alert alert-danger">
-                                <strong>Erreur</strong> compte deja existant
-                            </div>
-                        <?php 
-
-                    }
-                }
+               
                 ?>
             
             <form action="traitement_ins_employes.php" method="post">
                 <h2 class="text-center">Inscription des élèves</h2>   
                 <div class="form-group">
-                    <label for="statut">Prenom</label>
+                    <label for="prenom">Prenom</label>
                     <input type="text" name="prenom" class="form-control" placeholder="Prenom" required="required" autocomplete="off">
                 </div>
                 <div class="form-group">
-                    <label for="statut">Nom</label>
+                    <label for="nom">Nom</label>
                     <input type="text" name="nom" class="form-control" placeholder="Nom" required="required" autocomplete="off">
                 </div>
                 <div class="form-group">
