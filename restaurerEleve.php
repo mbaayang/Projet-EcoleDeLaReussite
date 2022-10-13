87bdb4fb19c46de49ea730dc237364213d165b21
zoukana <109985008+zoukana@users.noreply.github.com>
2022-10-13 15:01:25 +0000

    Add files via upload

---
+++ restaurerEleve.php
@@ -0,0 +1,12 @@
+<?php
+include('connect.php');
+if (isset($_GET['deleteid'])) {
+    $id=$_GET['deleteid'];
+    $datearchiver=date('y-m-d h:i:s');
+    $req=$bdd->prepare("UPDATE personnes SET archiver=0,  dateArchiver='$datearchiver' WHERE id='$id'");//code pour archiver en changeant la valeur 0 par 1
+    $req->execute();
+    if($req){
+       header('location:pageEleve.php');
+    }
+}
+?>
